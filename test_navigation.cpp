989a4379c63abd06291769ee56ff20aa25b28971
sllxgx <1348356527@qq.com>
2024-06-02 13:18:00 +0800

    分支new_branch第一次修改-新增了test_navigation.h和test_navigation.cpp文件

---
+++ test_navigation.cpp
@@ -0,0 +1,11 @@
+#include "test_navigation.h"
+
+void test_iostream()
+{
+    std::cout << "Hello world\n";
+}
+
+void test_navigaiton()
+{
+    std::cout << "Test navigation!" << std::endl;
+}
---
+++ test_navigation.h
@@ -0,0 +1,4 @@
+#include <iostream>
+
+void test_iostream();
+void test_navigaiton();
